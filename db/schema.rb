ca82323ad2a2873aaf46cf72c7a3ee21d7b383e9
Jack Wong <jackwong3101@yahoo.com>
2017-06-01 16:12:28 +0800

    C:/Program Files (x86)/Git

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170531151623) do
+ActiveRecord::Schema.define(version: 20170601045600) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"
@@ -65,6 +65,8 @@ ActiveRecord::Schema.define(version: 20170531151623) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "provider"
+    t.string   "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
